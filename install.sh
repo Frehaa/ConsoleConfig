4f8c29a893ee71ef88467c183469c94188a89e73
Frederik Haagensen <frehaa@hotmail.dk>
2019-05-29 12:28:07 +0200

    update install

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir ~/.vim/bundle
 sudo apt update
 sudo apt upgrade
 # Install
-sudo apt install vim git texlive-full cmake python3-dev build-essential trash-cli r-base gdebi-core keepass2 virtualenv htop
+sudo apt install vim git texlive-full cmake python3-dev build-essential trash-cli r-base gdebi-core keepass2 virtualenv htop libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 android-sdk 
 # Update & upgrade
 sudo apt update
 sudo apt upgrade
