535cbe4f179d8fb332449ab54e741fabaa8a7390
frehaa <frehaa@hotmail.dk>
2019-06-04 15:20:55 +0200

    Update tmux name

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -1,5 +1,5 @@
 cd ~/workspace/viewer
-tmux new -s react-native -d 
+tmux new -s stream-viewer -d 
 tmux split-window -h 
 tmux select-pane -t 0
 tmux split-window -v 
@@ -11,4 +11,4 @@ tmux select-pane -t 0
 tmux split-window -v 
 tmux select-pane -t 0
 htop
-tmux -2 attach -t react-native
+tmux -2 attach -t stream-viewer
