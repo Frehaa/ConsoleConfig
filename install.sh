79f70250b0e9b02d43922d0019ff11e69c04dc1a
Frederik Haagensen <frehaa@hotmail.dk>
2019-12-08 16:54:42 +0100

    Add ocamlfuse and remove r

--- install.sh
+++ install.sh
@@ -8,18 +8,21 @@ mkdir ~/.vim/bundle
 sudo apt update
 sudo apt upgrade
 # Install
-sudo apt install vim git texlive-full cmake python3-dev build-essential trash-cli r-base gdebi-core keepass2 virtualenv htop libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 nodejs nodejs-docs npm stack -y 
+sudo apt install vim git texlive-full cmake python3-dev build-essential trash-cli keepass2 virtualenv htop haskell-platform default-jdk -y 
 # Update & upgrade
 sudo apt update
 sudo apt upgrade
 # Get files
 cd ~/Downloads/
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-wget -q https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1335-amd64.deb
+# wget -q https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1335-amd64.deb
 # add apt
 sudo add-apt-repository "deb https://packages.microsoft.com/repos/vscode stable main"
 # Update & install code
 sudo apt update
 sudo apt install code
 # Update & install rstudio
-sudo gdebi rstudio-*.deb 
+#sudo gdebi rstudio-*.deb 
+sudo add-apt-repository ppa:alessandro-strada/ppa
+sudo apt update
+sudo apt install google-drive-ocamlfuse
