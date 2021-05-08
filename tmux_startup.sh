625b7682c78eb8fe098470d0edde062173ff106a
Frederik Haagensen <frehaa@hotmail.dk>
2021-05-08 08:02:31 +0200

    Update tmux startup script

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -1,6 +1,8 @@
 tmux new -s work -d 
-tmux split-window -h 
-tmux select-pane -t 0
 tmux split-window -v 
+tmux select-pane -t 0
+tmux split-window -h 
+tmux select-pane -t 2
+tmux split-window -h 
 tmux select-pane -t 0
 tmux -2 attach -t work
